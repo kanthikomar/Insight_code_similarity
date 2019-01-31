4966135e6410d437f3539a0b38d717d234186edd
Ameya Gamre <kanthikomar@gmail.com>
2019-01-31 10:06:02 -0600

    Added MinHash Example

--- preprocess_github_repos.py
+++ preprocess_github_repos.py
@@ -4,6 +4,8 @@
 #     count = count +1
 #     data = line.split(",")
 import csv
+import random
+import binascii
 # from smart_open import smart_open
 
 
@@ -11,6 +13,111 @@ import csv
 c_backslash = '\\'
 c_dquote = '"'
 c_comment = '#'
+
+maxShingleID = 2**32-1
+numHashes = 10
+nextPrime = 4294967311
+
+def pickRandomCoeffs(k):
+    # Create a list of 'k' random values.
+    randList = []
+
+    while k > 0:
+        # Get a random shingle ID.
+        randIndex = random.randint(0, maxShingleID)
+
+        # Ensure that each random number is unique.
+        while randIndex in randList:
+            randIndex = random.randint(0, maxShingleID)
+
+            # Add the random number to the list.
+        randList.append(randIndex)
+        k = k - 1
+
+    return randList
+
+# For each of the 'numHashes' hash functions, generate a different coefficient 'a' and 'b'.
+coeffA = pickRandomCoeffs(numHashes)
+coeffB = pickRandomCoeffs(numHashes)
+
+def genMinHashSign(docsAsShingleSets,docNames,):
+    # For each document...
+    signatures = []
+    for docID in docNames:
+
+        # Get the shingle set for this document.
+        shingleIDSet = docsAsShingleSets[docID]
+
+        # The resulting minhash signature for this document.
+        signature = []
+
+        # For each of the random hash functions...
+        for i in range(0, numHashes):
+
+            # For each of the shingles actually in the document, calculate its hash code
+            # using hash function 'i'.
+
+            # Track the lowest hash ID seen. Initialize 'minHashCode' to be greater than
+            # the maximum possible value output by the hash.
+            minHashCode = nextPrime + 1
+
+            # For each shingle in the document...
+            for shingleID in shingleIDSet:
+                # Evaluate the hash function.
+                hashCode = (coeffA[i] * shingleID + coeffB[i]) % nextPrime
+
+                # Track the lowest hash code seen.
+                if hashCode < minHashCode:
+                    minHashCode = hashCode
+
+            # Add the smallest hash code value as component number 'i' of the signature.
+            signature.append(minHashCode)
+
+        # Store the MinHash signature for this document.
+        signatures.append(signature)
+
+def generateShingles(Docs):
+    totalShingles = 0
+    numDocs = len(Docs)
+    docsAsShingleSets = {}
+    docNames = []
+    for i in range(0, numDocs):
+
+        # Read all of the words (they are all on one line) and split them by white
+        # space.
+        words = f.readline().split(" ")
+
+        # Retrieve the article ID, which is the first word on the line.
+        docID = words[0]
+
+        # Maintain a list of all document IDs.
+        docNames.append(docID)
+
+        del words[0]
+
+        # 'shinglesInDoc' will hold all of the unique shingle IDs present in the
+        # current document. If a shingle ID occurs multiple times in the document,
+        # it will only appear once in the set (this is a property of Python sets).
+        shinglesInDoc = set()
+
+        # For each word in the document...
+        for index in range(0, len(words) - 2):
+            # Construct the shingle text by combining three words together.
+            shingle = words[index] + " " + words[index + 1] + " " + words[index + 2]
+
+            # Hash the shingle to a 32-bit integer.
+            crc = binascii.crc32(shingle) & 0xffffffff
+
+            # Add the hash value to the list of shingles for the current document.
+            # Note that set objects will only add the value to the set if the set
+            # doesn't already contain it.
+            shinglesInDoc.add(crc)
+
+        # Store the completed list of shingles for this document in the dictionary.
+        docsAsShingleSets[docID] = shinglesInDoc
+
+        # Count the number of shingles across all documents.
+        totalShingles = totalShingles + (len(words) - 2)
 
 
 def chop_comment(line):
@@ -32,8 +139,9 @@ def chop_comment(line):
             in_quote = not in_quote
 
     return line
+codeDictionary = {}
 commentCharacter = "#"
-with open(r"C:\Users\asgam\PycharmProjects\insight_project\Data\results-20190124-124325.csv", encoding = "utf8") as csv_file:
+with open(r"results-20190124-124325.csv", encoding = "utf8") as csv_file:
     python_files = {}
     csv_reader = csv.reader(csv_file, delimiter=",")
     count = 0
@@ -63,8 +171,10 @@ with open(r"C:\Users\asgam\PycharmProjects\insight_project\Data\results-20190124
                     #     updatedCodeLen = len(updated_code)
                     # i+=1
 
-                print("after removing comments")
-                print(updated_code2)
+                # print("after removing comments")
+                # print(' '.join(updated_code2))
+                codeDictionary[id] = ' '.join(updated_code2)
+
                 # if not li.startswith("#"):
                 #     python_files[row[0]] = li
                 # python_files[row[0]] = row[2]
@@ -72,10 +182,11 @@ with open(r"C:\Users\asgam\PycharmProjects\insight_project\Data\results-20190124
 # print(python_files)
 # for y in python_files:
 #         print (y,':',python_files[y])
-
+print(codeDictionary)
+print(len(codeDictionary))
 with open('processed_data.txt', 'w',  encoding='utf-8') as f:
-    for key in python_files.keys():
-        f.write("%s %s\n"%(key,python_files[key]))
+    for key in codeDictionary.keys():
+        f.write("%s %s\n"%(key,codeDictionary[key]))
 
 
 # with smart_open('s3://samplecontentrepos/test_000000000000.csv', 'rb') as fin:
